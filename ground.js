c1780e36b6b3a43d68399ee6751ebb1ce228bc76
ISHITA-SHAW <83875590+ISHITA-SHAW@users.noreply.github.com>
2021-06-08 12:00:15 +0530

    Update ground.js

--- ground.js
+++ ground.js
@@ -1,21 +1,20 @@
-class ground{
-    constuctor(x,y,height,width){
+class Ground{
+    constructor(x,y,height,width){
+      
     var options = {
-    isStatic:true,
-    'restitution':0,
-    'friction':0,
-    'density':1
+    isStatic:true
     }
     this.body = Bodies.rectangle(x,y,width,height,options);
     this.width = width;
     this.height = height;
     World.add(world,this.body);
+      
     }
     display(){
-    rectMode(CENTER);
 
     fill(255);
-
-    rect(this.body.position.x,this.body.position.y,this.width,this.y);
-}
-};
+    rectMode(CENTER);
+    rect(this.body.position.x,this.body.position.y,this.width,this.height);
+    
+    }
+    };
